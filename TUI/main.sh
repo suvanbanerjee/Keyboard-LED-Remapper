8bb9bf597252ed8d5cd3702234edbc0692b31fdb
suvanbanerjee <banerjeesuvan@gmail.com>
2024-04-16 21:22:17 +0530

    some fixes in main.sh

--- TUI/main.sh
+++ TUI/main.sh
@@ -2,30 +2,20 @@
 
 source locales/us-en/strings.sh
 
-SESSION_TYPE= $XDG_SESSION_TYPE
-CHOICE=$(whiptail --title "$TITLE" --menu "$MENU" 25 78 16 \
-    "1" "Install Mod" \
-    "2" "Uninstall Mod" \
-    "3" "Test Keyboard" \
+SESSION_TYPE=$XDG_SESSION_TYPE
+
+CHOICE=$(whiptail --title "$TITLE" --cancel-button "Exit" --menu "$MENU" 15 50 4 \
+    1 "Install Mod" \
+    2 "Uninstall Mod" \
+    3 "Test Keyboard" \
     3>&1 1>&2 2>&3)
 
-case $CHOICE in
-    "1")
-        if [ "$SESSION_TYPE" == "tty" ]; then
-            echo "You are in a tty session, please switch to a graphical session to install the mod."
-            exit 1
-        fi
-        source TUI/config.sh
-        ;;
-    "2")
-        SESSION_TYPE="uninstall"
-        # TODO
-        pass
-        ;;
-    "3")
-        SESSION_TYPE="test"
-        source TUI/test.sh
-        source TUI/main.sh
-        ;;
-esac
-
+if [ "$CHOICE" == "1" ]; then
+    bash TUI/install_mod.sh
+elif [ "$CHOICE" == "2" ]; then
+    bash TUI/uninstall_mod.sh
+elif [ "$CHOICE" == "3" ]; then
+    bash TUI/test_keyboard.sh
+elif [ -z "$CHOICE" ]; then
+    exit 0
+fi
--- locales/us-en/strings.sh
+++ locales/us-en/strings.sh
@@ -1,5 +1,13 @@
-TITLE="Main Menu"
+#! bin/bash
 
-MENU="Choose one of the following options:"
+
+TITLE="Choose one of the following options:"
+
+MENU="
+
+Warning: This script is still in development and may not work as expected.
+Choose one of the following options:
+
+"
 
 export TITLE MENU
