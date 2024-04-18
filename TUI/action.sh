82c7a1cf671cb814281d60af21b56e1313c76c08
suvanbanerjee <banerjeesuvan@gmail.com>
2024-04-18 22:52:13 +0530

    small typo :)

--- TUI/action.sh
+++ TUI/action.sh
@@ -2,9 +2,9 @@
 
 source locales/us-en/strings.sh
 
-KEY=$(whiptail --title "$TITLE_ACTION" --menu "$ACTION" 15 50 4 \
+ACTION_KEY=$(whiptail --title "$TITLE_ACTION" --menu "$ACTION" 15 50 4 \
     "Disk Activity" "" \
     "Network Activity" "" \
     3>&1 1>&2 2>&3)
 
-export KEY
+export ACTION_KEY
