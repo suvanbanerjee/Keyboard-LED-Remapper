586536e706e97c15ef3a596a0b98db37f3f50d6f
suvanbanerjee <banerjeesuvan@gmail.com>
2024-04-24 18:00:23 +0530

    added exixting installer check

--- installer.sh
+++ installer.sh
@@ -28,6 +28,12 @@ fi
 git clone --quiet https://github.com/suvanbanerjee/Keyboard-LED-Remapper.git "$installer_path"
 cd "$installer_path" || exit 1
 
+#Check for exixting installation
+if [-f "$ls /etc/systemd/system/ | grep -E '(numlock|capslock|scrolllock).service'"]; then
+    echo -e "\n\e[31m[fail]\e[0m Existing installation found..."
+    echo -e "       Please uninstall the existing installation before running the installer.\n"
+    exit 1
+
 # Execute the installer entrypoint
 bash setup.sh "$@"
 
